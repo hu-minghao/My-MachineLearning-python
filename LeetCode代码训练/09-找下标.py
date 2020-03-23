a61985a03b1e0c23bdf7251dbbba6ca5132322a4
Minghao HU <45088266+hu-minghao@users.noreply.github.com>
2020-03-23 19:54:43 +0800

    Update 09-找下标.py

--- LeetCode代码训练/09-找下标.py
+++ LeetCode代码训练/09-找下标.py
@@ -11,3 +11,11 @@ class Solution:
                 res.append(dic[j])
                 res.append(dic[target-j])
         return res
+#解决了重复的问题
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dct = {}
+        for i, n in enumerate(nums):
+            if target - n in dct:
+                return [dct[target - n], i]
+            dct[n] = i
